00fc6506bf81959dcac422a9786f1b7193b54d27
jrock3 <jared@wolfgangweb.net>
2016-06-18 17:11:11 -0500

    Improved Angular

--- app.js
+++ app.js
@@ -2,16 +2,30 @@
   
 var app = angular.module('twitchApp', []);
   
-app.controller('TwitchCtrl', function($scope, $http) {
+app.controller('TwitchCtrl', ['$http', '$scope', function($http, $scope) {
 
-var check = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","brunofin"];   
+var check = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","brunofin", "ESL_SC2"];   
 $scope.list = [];
 $scope.status = "Status";
 
-angular.forEach(check, function(val) {
-  $scope.list.push(val);
-}); //end of forEach
+check.forEach(function(val) {
+  $http.jsonp('https://api.twitch.tv/kraken/channels/' + val + '?callback=JSON_CALLBACK').success(function(channelData) {
+        $http.jsonp('https://api.twitch.tv/kraken/streams/' + val + '?callback=JSON_CALLBACK').success(function(streamData) {
+          
+        var obj = {name: val};
+        if (streamData.stream === null) {
+        obj.status = "offline";
+      } else if (streamData.stream === undefined) {
+        obj.status = "no account";
+      } else {
+        obj.status = "ONLINE";
+      };
+        obj.logo = channelData.logo != null ? channelData.logo : "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+        $scope.list.push(obj);
+        }); //end stream
+  }); //end channel call
+});  //end forEach  
 
-}); /*end of controller*/
+}]); //end of controller
 
 })();
